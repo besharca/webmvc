b6c723717adfd54d5ef66e8618f86b8ee9c21832
besharca <puiugeorgealexandru@yahoo.com>
2018-12-03 20:02:07 +0200

    delete?

--- WebNewMysql/src/main/webapp/views/home/js/HelloWorld.js
+++
@@ -1,7 +0,0 @@
-/**
- * 
- */
-function calc(numA,numB){
-	return numA+numB;
-}
-
--- WebNewMysql/src/test/java/com/lil/demo/Testing.java
+++
@@ -1,9 +0,0 @@
-package com.lil.demo;
- 
-public class Testing {
-
-	public static void main(String[] args) throws Exception {
-		System.out.println("dabfdgd123900".matches("[a-zA-Z]"));
-	}
-
-}
